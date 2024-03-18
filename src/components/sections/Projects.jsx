557cd419ebbf06d95025d12b052c8e98a3ec0be2
Norlys Castañeda <castaneda30@hotmail.com>
2024-03-17 19:51:52 -0500

    add project

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import styled from 'styled-components';
-import useLang from '../../hooks/useLang';
-import useScrollReveal from '../../hooks/useScrollReveal';
-import GridItem from '../elements/GridItem';
-import Section from '../elements/Section';
-import StyledSubtitle from '../elements/Subtitle';
+import React from "react";
+import styled from "styled-components";
+import useLang from "../../hooks/useLang";
+import useScrollReveal from "../../hooks/useScrollReveal";
+import GridItem from "../elements/GridItem";
+import Section from "../elements/Section";
+import StyledSubtitle from "../elements/Subtitle";
 
 const StyledGrid = styled.div`
   width: 100%;
@@ -21,57 +21,65 @@ const StyledGrid = styled.div`
 
 const projects = [
   {
-    title: 'Ubuntu',
-    imageUrl: require('../../assets/images/Ubuntu.png'),
-    stack: ['React', 'MaterialUI', 'Formik'],
-    demoUrl: 'https://github.com/Norlysc/Ubuntu_02_front',
-    GithubUrl: 'https://github.com/Norlysc/Ubuntu_02_front',
-    id: 'left',
+    title: "Ubuntu",
+    imageUrl: require("../../assets/images/Ubuntu.png"),
+    stack: ["React", "MaterialUI", "Formik"],
+    demoUrl: "https://github.com/Norlysc/Ubuntu_02_front",
+    GithubUrl: "https://github.com/Norlysc/Ubuntu_02_front",
+    id: "left",
   },
   {
-    title: 'Nails studio',
-    imageUrl: require('../../assets/images/nails-studio.png'),
-    stack: ['React', 'Styled-components'],
-    demoUrl: 'https://norlysc.github.io/nails-studio/',
-    GithubUrl: 'https://github.com/Norlysc/nails-studio',
-    id: 'right',
+    title: "Nails studio",
+    imageUrl: require("../../assets/images/nails-studio.png"),
+    stack: ["React", "Styled-components"],
+    demoUrl: "https://norlysc.github.io/nails-studio/",
+    GithubUrl: "https://github.com/Norlysc/nails-studio",
+    id: "right",
   },
   {
-    title: 'Cinemo',
-    imageUrl: require('../../assets/images/cinemo.webp'),
-    stack: ['React', 'Css modules', 'Firebase'],
-    demoUrl: 'https://norlysc.github.io/cinemo/',
-    GithubUrl: 'https://github.com/Norlysc/cinemo',
-    id: 'left',
+    title: "Cinemo",
+    imageUrl: require("../../assets/images/cinemo.webp"),
+    stack: ["React", "Css modules", "Firebase"],
+    demoUrl: "https://norlysc.github.io/cinemo/",
+    GithubUrl: "https://github.com/Norlysc/cinemo",
+    id: "left",
   },
   {
-    title: 'e-commerce',
-    imageUrl: require('../../assets/images/e-commerce.png'),
-    stack: ['React', 'Styled-components', 'express'],
-    demoUrl: 'https://pool1541.github.io/e-commerce/',
-    GithubUrl: 'https://github.com/Norlysc/e-commerce',
-    id: 'right',
+    title: "E-commerce",
+    imageUrl: require("../../assets/images/e-commerce.png"),
+    stack: ["React", "Styled-components", "express"],
+    demoUrl: "https://pool1541.github.io/e-commerce/",
+    GithubUrl: "https://github.com/Norlysc/e-commerce",
+    id: "right",
+  },
+  {
+    title: "Calculadora",
+    imageUrl: require("../../assets/images/cal.png"),
+    stack: ["JavaScript", "HTML", "CSS"],
+    demoUrl: "https://norlysc.github.io/CALCULADORA-JS/",
+    GithubUrl: "https://github.com/Norlysc/CALCULADORA-JS",
+    id: "left",
   },
 ];
 
 export default function Projects() {
   const { lang } = useLang();
 
-  useScrollReveal(['#proyect-title'], {
-    origin: 'bottom',
-    distance: '50px',
+  useScrollReveal(["#proyect-title"], {
+    origin: "bottom",
+    distance: "50px",
   });
-  useScrollReveal(['#left'], {
-    origin: 'left',
-    distance: '50px',
+  useScrollReveal(["#left"], {
+    origin: "left",
+    distance: "50px",
   });
-  useScrollReveal(['#right'], {
-    origin: 'right',
-    distance: '50px',
+  useScrollReveal(["#right"], {
+    origin: "right",
+    distance: "50px",
   });
   return (
-    <Section id='projects'>
-      <StyledSubtitle id='proyect-title'>{lang.projects}</StyledSubtitle>
+    <Section id="projects">
+      <StyledSubtitle id="proyect-title">{lang.projects}</StyledSubtitle>
       <StyledGrid>
         {projects.map((proyect, index) => (
           <GridItem data={proyect} key={index} id={proyect.id} />
